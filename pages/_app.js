8d144df6fb252a42ca8c463c6bf0a3bbb810e6f6
Ioannis Psomadelis <ioannisp@bookonlinenow.net>
2023-07-06 13:17:00 +0300

    console

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import 'nextra-theme-blog/style.css'
 import Head from 'next/head'
-
+import Script from 'next/script'
 import '../styles/main.css'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function Nextra({ Component, pageProps }) {
   return (
@@ -13,15 +14,22 @@ export default function Nextra({ Component, pageProps }) {
           title="RSS"
           href="/feed.xml"
         />
-        <link
-          rel="preload"
-          href="/fonts/Inter-roman.latin.var.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
       </Head>
       <Component {...pageProps} />
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-V4SQL39NNY"
+      ></Script>
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-V4SQL39NNY');
+        `}
+      </Script>
+      <Analytics />
     </>
   )
 }
